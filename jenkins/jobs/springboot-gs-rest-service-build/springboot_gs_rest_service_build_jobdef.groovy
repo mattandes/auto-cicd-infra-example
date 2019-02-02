ecaced3d56a205527a14aaca495446623c248d38
Matt Andes <matt@mattandes.com>
2019-02-02 16:36:22 -0600

    Adding cleanBeforeCheckout option to git checkout.

--- jenkins/jobs/springboot-gs-rest-service-build/springboot_gs_rest_service_build_jobdef.groovy
+++ jenkins/jobs/springboot-gs-rest-service-build/springboot_gs_rest_service_build_jobdef.groovy
@@ -14,7 +14,9 @@ pipelineJob('springboot-gs-rest-service-build') {
           remote { url(repo) }
           branches('master')
           scriptPath('Jenkinsfile')
-          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+          extensions { 
+            cleanBeforeCheckout()
+          }
         }
       }
     }
